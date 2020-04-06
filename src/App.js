fe589e58f180e3408407bc646333a3f388555171
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-04-06 15:36:06 -0500

    adding map to application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Map from './components/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      center: [-98.4916, 29.4252]
+    }
+
+    this.changeCenter = this.changeCenter.bind(this);
+  }
+
+  changeCenter(long, lat){
+    let city = [long, lat];
+
+    this.setState({
+      center: city
+    })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <Map center={this.state.center} changeCityCenter={this.changeCenter} />
+      </div>
+    )
+  }
 }
-
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,44 @@
 import React, { Component } from 'react';
+import '../css/Map.css';
+import './geocode.js';
 
-const Map = () =>{
-    return(<div id="map"></div>)
+const mapboxKey = require('./keys.js');
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+mapboxgl.accessToken = mapboxKey;
+var marker = new mapboxgl.Marker({
+    draggable: true
+});
+
+class Map extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    onDragEnd(){
+        let lngLat = marker.getLngLat();
+        this.props.changeCityCenter(lngLat.lng, lngLat.lat);
+    }
+
+    componentDidMount(){
+        var mapCenter = this.props.center;
+
+        
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            zoom: 10,
+            center: mapCenter
+        });
+
+        marker.setLngLat(mapCenter);
+        marker.addTo(map);
+        marker.on('dragend', this.onDragEnd);
+    }
+
+    render(){
+        return(
+            <div id="map"></div>
+        )
+    }
 }
-
 export default Map;
